59d6d1e3057d472ceeef05dad49f794166ce9b4a
James Timms <Herman.Rogers@gmail.com>
2013-09-28 09:20:15 +0100

    scene transition changes

--- IndieSpeedRun/Assets/Scripts/BtnScripts/Utils/SceneForward.cs
+++ IndieSpeedRun/Assets/Scripts/BtnScripts/Utils/SceneForward.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public class SceneForward : MonoBehaviour {
 	void OnClick( ){
-		SceneFadeInOut.LoadLevel( ( Application.loadedLevel + 1 ),  0.3f, 0.3f, Color.blue );
+		SceneFadeInOut.LoadLevel( ( Application.loadedLevel + 1 ),  0.3f, 0.3f, Color.cyan );
 	}
 }
