3d993c1988fc977cd950cdc4e0605acccb9ae6d6
Boomer Rogers <booms@maintosh-7.local>
2013-09-29 00:11:30 +0100

    Main scene changes -- Elysia health bar

---
+++ IndieSpeedRun/Assets/Scripts/PlayerScripts/PlayerDamage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDamage : MonoBehaviour {
+	void OnTriggerEnter( ){
+		Debug.Log( tag );
+		if ( tag == "Enemy" ){
+			Debug.Log( "Enemy Damage" );
+		}
+	}
+	void OnCollisionEnter( ){
+		
+		Debug.Log( "Collide?" );
+		
+	}
+	
+}
