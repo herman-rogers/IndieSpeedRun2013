61f01b1c9ac5b1036be80c177aec004ddc48751e
James Timms <Herman.Rogers@gmail.com>
2013-09-29 12:17:29 +0100

    removed merges

--- IndieSpeedRun/Assets/Scripts/BtnScripts/Utils/SceneBack.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SceneBack : MonoBehaviour {
-	void OnClick( ){
-		SceneFadeInOut.LoadLevel( ( Application.loadedLevel - 1 ),  0.3f, 0.3f, Color.blue );
-	}
-}
