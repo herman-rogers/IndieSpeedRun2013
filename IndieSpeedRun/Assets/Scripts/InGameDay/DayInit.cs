6e5739ed306ebe7eac983bfc083bb57f592e2a39
James Timms <Herman.Rogers@gmail.com>
2013-09-29 17:09:47 +0100

    My changes plus conflict. DayTime scene messages added.

---
+++ IndieSpeedRun/Assets/Scripts/InGameDay/DayInit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayInit : MonoBehaviour {
+
+	void Start()
+	{
+		PlayerGlobals.isDay = true;
+		Destroy(this);
+	}
+	
+}
---
+++ IndieSpeedRun/Assets/Scripts/InGameDay/MenuDay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuDay : MonoBehaviour {
+	
+	public GameObject menu;
+	bool menuOpen = false;
+	
+	void OnMenuClicked()
+	{
+		menuOpen = !menuOpen;
+		NGUITools.SetActive(menu, menuOpen);
+		TweenAlpha tweenA = menu.GetComponent<TweenAlpha>();
+		tweenA.Play(menuOpen);
+		tweenA.enabled = true;
+	}
+	
+}
