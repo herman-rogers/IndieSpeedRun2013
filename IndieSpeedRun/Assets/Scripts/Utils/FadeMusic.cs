b695db6113e91815150d7769cbbe0265e9d810ed
Boomer Rogers <booms@maintosh-7.local>
2013-09-30 11:19:10 +0100

    small changes

--- IndieSpeedRun/Assets/Scripts/Utils/FadeMusic.cs
+++ IndieSpeedRun/Assets/Scripts/Utils/FadeMusic.cs
@@ -3,21 +3,21 @@ using System.Collections;
 
 public class FadeMusic : MonoBehaviour {
 	float volumeDown = 1;
-	float volumeUp   = 0;
+//	float volumeUp   = 0;
 	void Awake( ){
-		CancelInvoke( );
+		//CancelInvoke( );
 		InvokeRepeating( "FadeInMusic", 0.01f, 0.009f );
 	}
-	void OnClick( ){
-		CancelInvoke( );
-		InvokeRepeating( "FadeOutMusic", 0.01f, 0.001f);
-	}
+//	void OnClick( ){
+//		CancelInvoke( );
+//		InvokeRepeating( "FadeOutMusic", 0.01f, 0.001f);
+//	}
 	void FadeOutMusic( ){
 		volumeDown -= Time.deltaTime;
 		AudioListener.volume = volumeDown;
 	}
-	void FadeInMusic( ){
-		volumeUp += Time.deltaTime;
-		AudioListener.volume = volumeUp;
-	}
+//	void FadeInMusic( ){
+//		volumeUp += Time.deltaTime;
+//		AudioListener.volume = volumeUp;
+//	}
 }
